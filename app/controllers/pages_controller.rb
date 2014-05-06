53d8d1ae8ce2b0d1ecff47bb85c09994837ce2bb
Chris DeRose <cderose@bigboo.boca-colo.derosetechnologies.com>
2014-05-06 23:59:14 +0000

    Production mode fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,10 @@
-class PagesController < HighVoltage::PagesController
-  caches_page :show
+class PagesController < ApplicationController
+  COUNTIES_PATH = Rails.root.join('config', 'counties.yml')
+
+  caches_page :home
+
+  def home
+    @counties = YAML.load_file(COUNTIES_PATH)['counties'].collect{|county|
+      County.new(county) }
+  end
 end
